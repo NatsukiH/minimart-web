af0afad8dd85c28350a6c85ae38df0110be6f8a3
n h <natsu@enunoMacBook-Pro.local>
2021-08-16 18:03:01 +0900

    「カートページを作る」途中

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,36 +1,16 @@
 // カートに追加するデータの型
 // import {useRouter} from "next/router"
-import {Product, getProduct} from "../lib/product";
+// import {Product, getProduct} from "../lib/product";
 import { FC, useEffect, useState} from 'react';
 import { Layout } from "../components/Layout";
 
 
 const CartPage: FC = () => {
 
-    // const productID = router.query.id as string;
-    // const [product, setProduct ] = useState<Product| null>(null);
-    
-    // useEffect(
-    //     () => {
-    //         if(!productID){
-    //             return;
-    //         }
-    //         (async () => {
-    //             const prod = await getProduct(productID);
-    //             // console.log(prod);
-    //             setProduct(prod);
-                
-    //             localStorage.setItem('myCat', 'Tom');
-    //             localStorage.getItem(key)
-
-    //         })();
-    //     }, [ productID ]
-    // );
-
-
      return(
          <Layout>
             <h1>CartPage</h1>
+            <button onClick={() => window.alert("注文しました")}>注文する</button>
          </Layout>
      );
 } 
